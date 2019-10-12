7a56752188f49130d32e94467d268639c5c80522
Park, Joonsuu <parkjoonsuu@gmail.com>
2019-10-13 02:59:27 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+echo '********** virtualenv venv **********'
 echo `virtualenv venv`
 source "./venv/bin/activate"
+echo '********** pip **********'
 echo `pip install --upgrade pip`
+echo '********** requirements **********'
 echo `pip install -r requirements.txt`
+echo '********** setup.py **********'
 echo `pip install -e .`
-cd ./frontend
-echo `yarn install`
-echo `yarn build`
-cd ..
+echo '********** yarn install **********'
+echo `yarn install --cwd ./frontend`
+echo '********** yarn build **********'
+echo `cd "./frontend/" && yarn build --cwd ./frontend`
+echo `python manage.py migrate`
+echo '********** finished **********'
+echo 'run ("python manage.py runserver 0.0.0.0:8000")'
