9a6134d5044155453867ed94be79aacc45a5abe4
CodeSuu <parkjoonsuu@gmail.com>
2019-10-13 01:41:30 +0900

    set setup files

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ echo `virtualenv venv`
 source "./venv/bin/activate"
 echo `pip install --upgrade pip`
 echo `pip install -r requirements.txt`
+echo `pip install -e .`
 cd ./frontend
 echo `yarn install`
 echo `yarn build`
 cd ..
-echo `pip install -e .`
--- views/home.py
+++ views/home.py
@@ -6,13 +6,6 @@ from django.views.generic import TemplateView
 
 
 class ReactView(TemplateView):
-    def __init__(self, *args, **kwargs):
-        pass
-
     def get(self, request, *args, **kwargs):
-        import os
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        test = os.path.join(BASE_DIR, 'frontend', 'build'), # append
-        print(test)
         return render(request, 'index.html')
         
